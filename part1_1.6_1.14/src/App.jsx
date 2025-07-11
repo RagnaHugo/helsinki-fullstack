ed8f98b3280acd54fd9c1f84f770b367dfd2541a
RagnaHugo <123764597+RagnaHugo@users.noreply.github.com>
2025-07-11 10:51:35 -0500

    exercise 1.10

--- part1_1.6_1.14/src/App.jsx
+++ part1_1.6_1.14/src/App.jsx
@@ -25,29 +25,43 @@ const Statistics = ({ good, neutral, bad }) => {
   };
   return (
     <div>
-      <h2>good {good}</h2>
-      <h2>neutral {neutral}</h2>
-      <h2>bad {bad}</h2>
-      <h3>All {good + neutral + bad}</h3>
-      <h2>Average {calcAverage()}</h2>
-      <h2>Positive {calcPositive()} %</h2>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="all" value={good + neutral + bad} />
+      <StatisticLine text="average" value={calcAverage()} />
+      <StatisticLine text="positive" value={calcPositive()} />
     </div>
   );
 };
+
+function StatisticLine({ text, value }) {
+  return (
+    <>
+      <h2>
+        {text} {value}
+      </h2>
+    </>
+  );
+}
 
 const App = () => {
   // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-
+  let total = good + neutral + bad;
   return (
     <div>
       <h1>Give feedback</h1>
       <Button onIncrement={setGood} name="Good" />
       <Button onIncrement={setNeutral} name="Neutral" />
       <Button onIncrement={setBad} name="Bad" />
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      {total !== 0 ? (
+        <Statistics good={good} neutral={neutral} bad={bad} />
+      ) : (
+        <h2>No feedback Given</h2>
+      )}
     </div>
   );
 };
